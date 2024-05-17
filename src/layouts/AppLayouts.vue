a611d70a38e2910ebd4f1ddaa82c4012ac02c87c
Folkwayse <folkwayse@gmail.com>
2024-05-17 11:21:52 +0700

    setting app layout

--- src/layouts/AppLayouts.vue
+++ src/layouts/AppLayouts.vue
@@ -29,7 +29,7 @@
         <a-layout-content
           :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
         >
-          Content
+          <router-view></router-view>
         </a-layout-content>
       </a-layout>
     </a-layout>
